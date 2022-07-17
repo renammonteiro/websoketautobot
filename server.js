26c13d42ca1c278e091b9ea1182a2c668bf7faa4
Renam Monteiro <renammaveric@gmail.com>
2022-07-16 21:41:37 -0300

    update avisa quando usuario saiu da conversar

--- server.js
+++ server.js
@@ -32,12 +32,12 @@ serverSocket.on('connect', function(socket){
     })
 
     socket.on('disconnect', function(){
-        console.log('Cliente desconectado: ' + socket.nickname)
-        serverSocket.emit('chat msg', 'Cliente desconectado: ' + socket.nickname)
+        console.log(`Cliente desconectado: ${socket.nickname}`)
+        serverSocket.emit('chat msg', `${socket.nickname} saiu` )
     })
         
     socket.on('chat msg', function(msg){
-        serverSocket.emit('chat msg', `${socket.nickname} diz: ${msg}`)
+        serverSocket.emit('chat msg', JSON.stringify(socket)+`${socket.nickname} diz: ${msg}`)
     })
 
     socket.on('status', function(msg){
